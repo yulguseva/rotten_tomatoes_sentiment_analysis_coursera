c5ae7f01934b7bbd2d9cedb1fc661dc34722d0a6
yulguseva <33717702+yulguseva@users.noreply.github.com>
2018-09-20 09:25:15 +0100

    Added comments on model performance

--- bigru-sentiment-analysis.py
+++ bigru-sentiment-analysis.py
@@ -1,6 +1,5 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+#Model with 2 hidden layers of Bidirectional GRU. 
+#This solution aims for 63.4 accuracy on test set for 5-class text classification.
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
